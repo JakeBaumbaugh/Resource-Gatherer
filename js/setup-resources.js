ea92ac7b39cfa035b9f595e7c5a08eb04fedb96c
Jake Jake <jake.baumbaugh@gmail.com>
2021-04-30 15:05:39 -0400

    implemented prestige system to increase stockpile capacity

--- js/setup-resources.js
+++ js/setup-resources.js
@@ -1,17 +1,12 @@
 var money = 0;
-var resources = [
-    new Resource("wood", "Wood", "#634F2E", "axe", "Axe"),
-    new Resource("stone", "Stone", "#888888", "pickaxe", "Pickaxe"),
-];
+var prestigePoints = 0;
+var prestigePointCost = 100;
+var resources = null;
 
 window.onload = function() {
     setupLinks();
-
-    for(resource of resources) {
-        resource.buildResourceDisplay();
-        resource.buildMarketDisplay();
-        resource.buildBlacksmithDisplay();
-    }
+    setupResources();
+    setupPrestige();
 }
 
 function setupLinks() {
@@ -32,10 +27,71 @@ function setupLinks() {
     linkClick.call(sections[0]);
 }
 
+function setupResources() {
+    // Clear generated resource divs
+    if(resources != null) {
+        for(resource of resources) {
+            document.getElementById(resource.name+"-container").remove();
+            document.getElementById(resource.name+"-market-container").remove();
+            document.getElementById(resource.name+"-blacksmith-container").remove();
+        }
+    }
+    // Setup values and HTML
+    resources = [
+        new Resource("wood", "Wood", "#634F2E", "axe", "Axe"),
+        new Resource("stone", "Stone", "#888888", "pickaxe", "Pickaxe"),
+    ];
+    for(resource of resources) {
+        resource.buildResourceDisplay();
+        resource.buildMarketDisplay();
+        resource.buildBlacksmithDisplay();
+    }
+}
+
+function setupPrestige() {
+    document.getElementById("prestige-button").onclick = prestigeClick;
+}
+
 function updateMoney() {
+    // Update money values
     let elem;
     for(let i=0; (elem=document.getElementById("money-span-"+i)) != null; i++) {
         elem.innerHTML = money;
+    }
+    // Update prestige gain
+    document.getElementById("prestige-gain-span").innerHTML = prestigeGain();
+    document.getElementById("prestige-next-span").innerHTML = nextPrestigePoint();
+}
+
+function prestigeClick() {
+    // Make sure prestigeGain > 0
+    if(prestigeGain() == 0) {
+        return;
+    }
+    prestigePoints += prestigeGain();
+    money = 0;
+    document.getElementById("prestige-span").innerHTML = prestigePoints;
+    document.getElementById("prestige-boost-span").innerHTML = capBoost();
+    setupResources();
+    updateMoney();
+}
+
+function capBoost() {
+    return 2 ** prestigePoints;
+}
+
+function prestigeGain() {
+    let gain = Math.floor(Math.log(money) / Math.log(10)) - 1;
+    return Math.max(gain, 0);
+}
+
+function nextPrestigePoint() {
+    let log = Math.log(money) / Math.log(10)
+    let dec = log % 1;
+    if(dec == 0) {
+        return Math.max(10 ** (log+1), 100);
+    } else {
+        return Math.max(10 ** Math.ceil(log), 100);
     }
 }
 
@@ -45,7 +101,7 @@ function Resource(name, displayName, color, upgradeName, upgradeDisplayName) {
     this.color = color;
     this.price = 1;
     this.incr = 1;
-    this.cap = 100;
+    this.cap = 100 * capBoost();
     this.count = 0;
 
     this.upgradeName = upgradeName;
